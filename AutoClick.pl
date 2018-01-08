377c2a9b2c90ca80422bcfd51b9e6be9032bd061
N3150DC-ITX\Nick-IB <1135066@gmail.com>
2018-01-08 10:38:40 +0900

    様々な改良

--- AutoClick.pl
+++ AutoClick.pl
@@ -13,8 +13,6 @@ my $sel = WWW::Selenium->new(
     browser_url => "http://orteil.dashnet.org/",
 );
 
-my $tm0 = time();
-
 $sel->start;
 $sel->open("/cookieclicker/");
 my $savedata = '';
@@ -28,70 +26,94 @@ while (1) {
 
 my $locator_textarea_prompt = "id=textareaPrompt";
 my $locator_prompt_option0 = "id=promptOption0";
+
+while ( 1 ) {
+    next if ( !&is_enabled($locator_textarea_prompt) );
+    next if ( !$sel->is_visible($locator_textarea_prompt) );
+    next if ( !$sel->is_visible($locator_prompt_option0) );
+
+    my $text = &get_text($locator_textarea_prompt);
+    my $btnText = &get_text($locator_prompt_option0);
+
+    next if ( $text ne '' );
+    next if ( $btnText ne 'Load' );
+
+    open(my $fh, "<", "savedata.txt") or die $!;
+    while ( defined(my $l = <$fh>) ) {
+        chomp $l;
+        if ( $l ne '' ) {
+            $savedata = $l;
+        }
+    }
+    close $fh;
+
+    $sel->type($locator_textarea_prompt, $savedata);
+    &click($locator_prompt_option0);
+    sleep 2;
+    last;
+}
+
 my $locator_golden_cookie = 'xpath=//div[@class="shimmer"]';
 my $locator_big_cookie = "id=bigCookie";
-my $locator_season_popup = "id=seasonPopup";
 my $locator_export = '//a[text()="Export save"]';
+my $tm = time();
 
 while ( 1 ) {
     try {
-        if ( &is_enable($locator_textarea_prompt) &&
-                $sel->is_visible($locator_textarea_prompt) &&
-                $sel->is_visible($locator_prompt_option0) ) {
-            my $text = &get_text($locator_textarea_prompt);
-            my $btnText = &get_text($locator_prompt_option0);
+        if ( &click($locator_golden_cookie, "goldenCookie") + &click($locator_golden_cookie, "goldenCookie") ) {
+            if ($sel->get_eval("window.Game.hasBuff('Cookie storm')")) {
+                my $err = '';
+                for (1 .. 10) {
+                    $sel->click($locator_golden_cookie); $sel->click($locator_golden_cookie); $sel->click($locator_golden_cookie); $sel->click($locator_golden_cookie);
+                    $sel->click($locator_golden_cookie); $sel->click($locator_golden_cookie); $sel->click($locator_golden_cookie); $sel->click($locator_golden_cookie);
+                    $sel->click($locator_golden_cookie); $sel->click($locator_golden_cookie); $sel->click($locator_golden_cookie); $sel->click($locator_golden_cookie);
+                    $sel->click($locator_golden_cookie); $sel->click($locator_golden_cookie); $sel->click($locator_golden_cookie); $sel->click($locator_golden_cookie);
+                    $sel->click($locator_golden_cookie); $sel->click($locator_golden_cookie); $sel->click($locator_golden_cookie); $sel->click($locator_golden_cookie);
+                    $sel->click($locator_golden_cookie); $sel->click($locator_golden_cookie); $sel->click($locator_golden_cookie); $sel->click($locator_golden_cookie);
+                    $sel->click($locator_golden_cookie); $sel->click($locator_golden_cookie); $sel->click($locator_golden_cookie); $sel->click($locator_golden_cookie);
+                    $sel->click($locator_golden_cookie); $sel->click($locator_golden_cookie); $sel->click($locator_golden_cookie); $sel->click($locator_golden_cookie);
+                    $sel->click($locator_golden_cookie); $sel->click($locator_golden_cookie); $sel->click($locator_golden_cookie); $sel->click($locator_golden_cookie);
+                    $sel->click($locator_golden_cookie); $sel->click($locator_golden_cookie); $sel->click($locator_golden_cookie); $sel->click($locator_golden_cookie);
+                    $sel->click($locator_golden_cookie); $sel->click($locator_golden_cookie); $sel->click($locator_golden_cookie); $sel->click($locator_golden_cookie);
+                    $sel->click($locator_golden_cookie); $sel->click($locator_golden_cookie); $sel->click($locator_golden_cookie); $sel->click($locator_golden_cookie);
+                    $sel->click($locator_golden_cookie); $sel->click($locator_golden_cookie); $sel->click($locator_golden_cookie); $sel->click($locator_golden_cookie);
+                    $sel->click($locator_golden_cookie); $sel->click($locator_golden_cookie); $sel->click($locator_golden_cookie); $sel->click($locator_golden_cookie);
+                    $sel->click($locator_golden_cookie); $sel->click($locator_golden_cookie); $sel->click($locator_golden_cookie); $sel->click($locator_golden_cookie);
+                    $sel->click($locator_golden_cookie); $sel->click($locator_golden_cookie); $sel->click($locator_golden_cookie); $sel->click($locator_golden_cookie);
+                    $sel->click($locator_golden_cookie); $sel->click($locator_golden_cookie); $sel->click($locator_golden_cookie); $sel->click($locator_golden_cookie);
+                    $sel->click($locator_golden_cookie); $sel->click($locator_golden_cookie); $sel->click($locator_golden_cookie); $sel->click($locator_golden_cookie);
+                    $sel->click($locator_golden_cookie); $sel->click($locator_golden_cookie); $sel->click($locator_golden_cookie); $sel->click($locator_golden_cookie);
+                    $sel->click($locator_golden_cookie); $sel->click($locator_golden_cookie); $sel->click($locator_golden_cookie); $sel->click($locator_golden_cookie);
+                    $sel->click($locator_golden_cookie); $sel->click($locator_golden_cookie); $sel->click($locator_golden_cookie); $sel->click($locator_golden_cookie);
+                    $sel->click($locator_golden_cookie); $sel->click($locator_golden_cookie); $sel->click($locator_golden_cookie); $sel->click($locator_golden_cookie);
+                    $sel->click($locator_golden_cookie); $sel->click($locator_golden_cookie); $sel->click($locator_golden_cookie); $sel->click($locator_golden_cookie);
+                    $sel->click($locator_golden_cookie); $sel->click($locator_golden_cookie); $sel->click($locator_golden_cookie); $sel->click($locator_golden_cookie);
+                    $sel->click($locator_golden_cookie); $sel->click($locator_golden_cookie); $sel->click($locator_golden_cookie); $sel->click($locator_golden_cookie);
+                    print "\nclick cookie storming";
+                }
+            }
 
-            if ( $btnText eq 'All done!' && $savedata ne $text ) {
-                $savedata = $text;
+            if ( $tm + 600 < time() ) {
+                $savedata = $sel->get_eval("window.localStorage.getItem('CookieClickerGame')");
                 open my $fh, ">>", "savedata.txt" or die $!;
                 print $fh "$savedata\n";
                 close $fh;
-                while ( !&click($locator_prompt_option0) ) {
-                    next;
-                }
-            } elsif ( $btnText eq 'Load' && $text eq '' ) {
-                open(my $fh, "<", "savedata.txt") or die $!;
-                while ( defined(my $l = <$fh>) ) {
-                    chomp $l;
-                    if ( $l ne '' ) {
-                        $savedata = $l;
-                    }
-                }
-                close $fh;
-                while ( 1 ) {
-                    if ( $sel->is_visible("id=textareaPrompt") &&
-                            $sel->is_visible($locator_prompt_option0) ) {
-                        $sel->type("id=textareaPrompt", $savedata);
-                        &click($locator_prompt_option0);
-                        sleep 2;
-                        # &click("id=storeBulk10");
-                        last;
-                    }
-                }
-            }
-            if ( $sel->get_eval("window.Game.elderWrath") ) {
-                &explode_wrinklers();
+                $tm = time();
+                my ($sec, $min, $hour) = localtime($tm);
+                printf("\nsaved(%02d:%02d:%02d)", $hour, $min, $sec);
             }
         }
     } catch {
-        print "ERROR: $_";
+        print "ERROR(while): $_";
     };
 
-    if ( $sel->is_element_present($locator_golden_cookie) && &click($locator_golden_cookie, "goldenCookie") ) {
-        while ($sel->get_eval("window.Game.hasBuff('Cookie storm')")) {
-            &click($locator_golden_cookie, "cookie storming");
-        }
-    }
-
-    &click($locator_season_popup, "seasonPopup");
-
     &buy_upgrade();
+    &buy_product();
 
-    my $tm = time();
-    if ( $tm - $tm0 >= 300 ) {
-        &to_menu_and_click($locator_export, "export($tm)");
-        $tm0 = $tm;
+    if ( $sel->get_eval("window.Game.elderWrath") ) {
+        &explode_wrinklers();
     }
+    print ".";
 }
 
 sub to_menu_and_click {
@@ -99,7 +121,7 @@ sub to_menu_and_click {
     my $target_name = shift;
     my $locator_menu_button = '//div[@id="prefsButton"]';
 
-    while (!&is_enable($locator)) {
+    while (!&is_enabled($locator)) {
         &click($locator_menu_button, "menu");
     }
     &click($locator, $target_name);
@@ -107,46 +129,36 @@ sub to_menu_and_click {
 
 sub buy_upgrade {
     my $locator = '//div[@id="upgrades"]/div[@class="crate upgrade enabled"][1]';
-    if ( &is_enable($locator) ) {
-        &click($locator, "upgrade");
-    }
+    &click($locator, "upgrade");
 }
 
-sub is_enable {
+sub buy_product {
+    my $locator = '//div[@id="products"]/div[@class="product unlocked enabled"][last()]';
+    &click($locator, "product");
+}
+
+sub is_enabled {
     my $locator = shift;
     return $sel->is_element_present($locator);
 }
 
 sub explode_wrinklers {
     $sel->get_eval("window.Game.SaveWrinklers().number < window.Game.getWrinklersMax() || window.Game.CollectWrinklers()");
-    print "explode wrinklers\n";
 }
 
 sub click {
     my $locator = shift;
     my $target_name = shift;
-    my $rtn = 0;
+    my $rtn;
 
     try {
-        if ( $sel->is_visible($locator) ) {
-            $sel->click($locator);
-            print "click $target_name\n" if ( $target_name );
-        }
+        $sel->click($locator);
+        print "\nclick $target_name" if ( $target_name );
         $rtn = 1;
     } catch {
-        print "ERROR: $_";
+        $rtn = 0;
     };
     return $rtn;
-}
-
-sub double_click {
-    my $locator = shift;
-
-    try {
-        $sel->double_click($locator);
-    } catch {
-        print "ERROR: $_";
-    };
 }
 
 sub get_text {
@@ -157,7 +169,7 @@ sub get_text {
         try {
             $text = $sel->get_text($locator);
         } catch {
-            print "ERROR: $_\n";
+            print "\nERROR(get_text): $_";
         };
     }
 
--- runSeleniumServer.bat
+++ runSeleniumServer.bat
@@ -1,4 +1,6 @@
 :java -jar selenium-server-standalone-2.42.2.jar -timeout 86400
 :java -jar selenium-server-standalone-2.52.0.jar -timeout 86400
 java -jar selenium-server-standalone-2.53.0.jar -timeout 86400
+:java -jar selenium-server-standalone-3.0.1.jar -timeout 86400
+:java -jar selenium-server-standalone-3.8.1.jar -timeout 86400
 pause
